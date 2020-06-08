03ab6e5cfd2a2bac603cb37941272bd4d7f451e7
zinnqu <quinton.zinn@centurylink.com>
2020-06-08 22:30:17 +0000

    Lab 2 part 1 of 2

--- lab02.part1.main.js
+++ lab02.part1.main.js
@@ -11,15 +11,13 @@ const IPCIDR = require('ip-cidr');
  * @param {string} cidrStr - The IPv4 subnet expressed
  *                 in CIDR format.
  * @param {callback} callback - A callback function.
- * @return {string, string} (firstIpAddress, ipv6Address) - An IPv4 address and an IPv6 address
+ * @return {string} (firstIpAddress) - An IPv4 address.
  */
 function getFirstIpAddress(cidrStr, callback) {
 
   // Initialize return arguments for callback
   let firstIpAddress = null;
   let callbackError = null;
-  let ipv6Address = null;
-  let returnData = null;
 
   // Instantiate an object from the imported class and assign the instance to variable cidr.
   const cidr = new IPCIDR(cidrStr);
@@ -39,19 +37,12 @@ function getFirstIpAddress(cidrStr, callback) {
     // If the passed CIDR is valid, call the object's toArray() method.
     // Notice the destructering assignment syntax to get the value of the first array's element.
     [firstIpAddress] = cidr.toArray(options);
-    ipv6Address = getIpv4MappedIpv6Address(firstIpAddress);
   }
   // Call the passed callback function.
   // Node.js convention is to pass error data as the first argument to a callback.
   // The IAP convention is to pass returned data as the first argument and error
   // data as the second argument to the callback function.
-  if (firstIpAddress === null){
-      returnData = {"ipv4": null, "ipv6": null};
-  } else {
-      returnData = {"ipv4": '"'+ firstIpAddress +'"', "ipv6": '"'+ipv6Address+'"'};
-  }
-  //returnData = {"ipv4": firstIpAddress, "ipv6": ipv6Address};
-  return callback(returnData, callbackError);
+  return callback(firstIpAddress, callbackError);
 }
 
 /**
@@ -126,7 +117,7 @@ function main() {
       if (error) {
         console.error(`  Error returned from GET request: ${error}`);
       }
-      console.log(`  Response returned from GET request: {"ipv4":${data.ipv4},"ipv6":${data.ipv6}}`);
+      console.log(`  Response returned from GET request: ${data}`);
     });
   }
   // Iterate over sampleIpv4s and pass the element's value to getIpv4MappedIpv6Address().
